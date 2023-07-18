8712a4775f3e1398956800d2bff648fb72052514
LilNex <nextorchq@gmail.com>
2023-07-18 19:20:42 +0100

    Another one

--- src/minishell.c
+++ src/minishell.c
@@ -6,18 +6,13 @@
 /*   By: ichaiq <ichaiq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 21:56:47 by ahallali          #+#    #+#             */
-/*   Updated: 2023/07/18 19:18:24 by ichaiq           ###   ########.fr       */
+/*   Updated: 2023/07/18 19:19:18 by ichaiq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 t_minishell	*g_minishell;
-
-void f()
-{
-	system("leaks minishell");
-}
 
 void f()
 {
@@ -52,18 +47,18 @@ int	prompt_and_exec(t_parse_utils *p_prompt, char *line)
 	return (0);
 }
 
-	(void)ac;
-	(void)av;
-	line = NULL;
-	g_minishell = ft_calloc(1, sizeof(t_minishell));
-	if (!g_minishell)
-		return (0);
-	if (*env)
-		g_minishell->env = ft_env(env, g_minishell);
-	else
-	g_minishell->env = ft_empty();
-	return (1);
-}
+// 	(void)ac;
+// 	(void)av;
+// 	line = NULL;
+// 	g_minishell = ft_calloc(1, sizeof(t_minishell));
+// 	if (!g_minishell)
+// 		return (0);
+// 	if (*env)
+// 		g_minishell->env = ft_env(env, g_minishell);
+// 	else
+// 	g_minishell->env = ft_empty();
+// 	return (1);
+// }
 
 int	main(int ac, char **av, char **env)
 {
